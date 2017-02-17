8b1d186c9bb831e782a00571a4511116569bc93d
Tinu Dahiya <tinu.dahiya@indianic.com>
2017-02-17 12:55:19 +0530

    Project added

---
+++ TDTagListCollection/TDTagListCollection/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TDTagListCollection
+//
+//  Created by thedahiyaboy on 23/11/16.
+//  Copyright © 2016 thedahiyaboy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
